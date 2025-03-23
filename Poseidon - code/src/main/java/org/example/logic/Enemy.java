489dece8654b670881135b424cad0f3607c54f03
LukasKukkuk <87575339+LukasKukkuk@users.noreply.github.com>
2025-03-23 11:48:56 +0100

    Delete Poseidon - code directory

--- Poseidon - code/src/main/java/org/example/logic/Enemy.java
+++
@@ -1,9 +0,0 @@
-package org.example.logic;
-
-public class Enemy extends Entity {
-
-    public Enemy(int x, int y, String url) {
-        super(x, y, url);
-    }
-
-}
--- Poseidon - code/src/main/java/org/example/logic/PNG.java
+++
@@ -1,7 +0,0 @@
-package org.example.logic;
-
-public class PNG extends Entity {
-    public PNG(int x, int y, String url) {
-        super(x, y, url);
-    }
-}
